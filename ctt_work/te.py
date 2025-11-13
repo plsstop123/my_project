a192bc76998cb7c4644e23ff66643975de9d61a3
plsstop123 <25021536@vnu.edu.vn>
2025-11-13 14:44:08 +0700

    hhhh

--- ctt_work/te.py
+++ ctt_work/te.py
@@ -1,9 +1,8 @@
-giasachtoan = 15
-giasachvan = 11
-print("Số lượng sách toán:")
-soluongtoan = int(input())
-print("Số lượng sách văn:")
-soluongvan = int(input())
-tongtien = (giasachtoan*soluongtoan) + (giasachvan*soluongvan)
-print("Tổng số tiền là:")
-print(tongtien)
+import math 
+
+x=1.1 + 2.2
+print(x==3.3)
+print(1.1 + 2.2)
+
+print ("-"*30)
+print(math.isclose(x, 3.3))
