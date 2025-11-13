a518207877717cf1863708e04edd2da3ba17020e
plsstop123 <25021536@vnu.edu.vn>
2025-11-13 14:43:11 +0700

    hhh

---
+++ ctt_work/te.py
@@ -0,0 +1,9 @@
+giasachtoan = 15
+giasachvan = 11
+print("Số lượng sách toán:")
+soluongtoan = int(input())
+print("Số lượng sách văn:")
+soluongvan = int(input())
+tongtien = (giasachtoan*soluongtoan) + (giasachvan*soluongvan)
+print("Tổng số tiền là:")
+print(tongtien)
